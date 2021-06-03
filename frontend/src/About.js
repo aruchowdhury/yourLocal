f777e1943050931922db8a5482e6d4046ee92a88
ArunavaChowdhury <arunav.pavel@gmail.com>
2021-06-03 17:00:38 -0400

    About and Contact page in progress

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,7 +1,80 @@
 import React from "react";
+import styled from "styled-components";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Background>
+      <Wrapper>
+        <Para>
+          <h1>Why yourL🍔CAL:</h1>
+          <p>
+            According to{" "}
+            <a
+              style={{ color: "black" }}
+              href="https://www.restaurantscanada.org/support-restaurants/"
+              alt="restaurantscanada.org web url"
+            >
+              https://www.restaurantscanada.org/support-restaurants/
+            </a>{" "}
+            since march 2020 10,000 restaurants closed due to a global pandemic.
+            A big portion of those closed restaurants is locally owned small
+            businesses. So it was never been a hard time for our local business
+            owners than now ever.
+          </p>
+          <p>
+            After joining Concordia Bootcamp I was thinking why not make a
+            product to help this group of people and also help consumers to find
+            specific local or ethnic food they are looking for. So I decided to
+            make an application where all local and ethnic restaurants can have
+            their owned web presence, food ordering system, and much more. And
+            for consumers, you are looking for all sushi shops or smoked burger
+            shop in your locality or maybe you are looking for authentic Italian
+            restaurants, now can you get them all in one place and can order
+            from them.{" "}
+          </p>
+          <p>
+            To make this project a real product I contacted 2 local restaurants
+            and they responded really well. But they don’t have enough digital
+            photos of foods that I would require for my application ASAP. So I
+            have decided to make my application with mock photos and those 2
+            restaurant owners are really interested and wants to work with me in
+            future.
+          </p>{" "}
+          <p>
+            Now I would like to present my product yourLocals. It is a food
+            order and restaurant management SAAS application.
+          </p>
+        </Para>
+      </Wrapper>
+    </Background>
+  );
 };
 
+const Background = styled.div`
+  background-color: #b97e00;
+  margin: auto;
+  width: 85vw;
+  height: 80vh;
+  align-content: center;
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: auto;
+  border-radius: 1.5rem;
+  background-color: #ffae01;
+  width: 60rem;
+  height: 30rem;
+  color: black;
+`;
+const Para = styled.p`
+  font-size: 1.2rem;
+  color: black;
+  padding: 5rem;
+`;
 export default About;
--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,7 +1,204 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import img from "./assets/bg.png";
+import { GiCandlestickPhone } from "react-icons/gi";
+import { BiEnvelopeOpen } from "react-icons/bi";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Background>
+      <Wrapper>
+        <ContactInfo>
+          <h3>
+            <Envelope>
+              <BiEnvelopeOpen />
+            </Envelope>
+            CONTACT <HomeLink to="/">US</HomeLink>
+          </h3>
+
+          <div>
+            <p>
+              <GiCandlestickPhone /> +1 222 333 4444
+            </p>
+            <p> 111-0000 Rue Abc, Montreal</p>
+            <p>A0B 0C0, QC</p>
+          </div>
+        </ContactInfo>
+        <ContactForm>
+          <h2>Contact Form</h2>
+          <form action="submit">
+            <NameDiv>
+              <div>
+                <label>First Name:</label>
+                <br />
+                <Input
+                  type="text"
+                  name="firstname"
+                  placeholder="Your First Name"
+                  required
+                />
+              </div>
+              <div>
+                <label>Last Name:</label>
+                <br />
+                <Input
+                  type="text"
+                  name="firstname"
+                  placeholder="Your Last Name"
+                  required
+                />
+              </div>
+            </NameDiv>{" "}
+            <br />
+            <label>Telephone:</label>
+            <br />
+            <Input
+              type="number"
+              name="telephone"
+              placeholder="Your telephone no"
+              maxlength="10"
+              required
+            />
+            <br />
+            <label>Email:</label>
+            <br />
+            <Input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              required
+            />
+            <br />
+            <label>Message:</label>
+            <br />
+            <MessageInput
+              type="text"
+              name="message"
+              placeholder="Write your message..."
+              required
+            />
+            <br />
+            <ButtonDiv>
+              <Button>Clear</Button>
+              <Button>Send</Button>
+            </ButtonDiv>
+          </form>
+        </ContactForm>
+      </Wrapper>
+    </Background>
+  );
 };
+
+const Background = styled.div`
+  background-color: #b97e00;
+  margin: auto;
+  width: 85vw;
+  height: 80vh;
+  align-content: center;
+  align-items: center;
+  /* background-image: url(${img});
+  background-size: cover; */
+  position: relative;
+  z-index: -1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: auto;
+  border-radius: 1.5rem;
+  background-color: #ffae01;
+  width: 60rem;
+  height: 30rem;
+  color: black;
+`;
+
+const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: left;
+  padding: 2rem;
+`;
+const ContactForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: left;
+  padding: 2rem;
+`;
+
+const HomeLink = styled(NavLink)`
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+  &:hover {
+    color: white;
+  }
+`;
+
+const Envelope = styled.div`
+  font-size: 5rem;
+`;
+
+const NameDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: left;
+  align-content: space-around;
+`;
+
+const Input = styled.input`
+  width: 12rem;
+  padding: 0.3rem;
+  border-radius: 0.3rem;
+  margin: 0.2rem 0.2rem 0.2rem 0;
+`;
+const MessageInput = styled.input`
+  width: 25rem;
+  height: 4rem;
+  padding: 0.3rem;
+  border-radius: 0.3rem;
+  margin: 0.2rem 0;
+
+  @media (max-width: 600px) {
+    width: 12rem;
+    height: 10rem;
+  }
+`;
+
+const ButtonDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  align-items: right;
+  align-content: space-around;
+`;
+
+const Button = styled.button`
+  height: 2.2rem;
+  width: 5rem;
+  margin: 0.5rem 0 0.5rem 0.5rem;
+  background: black;
+  border: 0.1rem solid black;
+  border-radius: 0.4rem;
+  color: #ffae01;
+
+  &:hover {
+    background: #ffae01;
+    border: 0.1rem solid black;
+    border-radius: 0.4rem;
+    color: black;
+  }
+`;
 
 export default Contact;
